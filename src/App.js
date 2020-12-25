07af1f74819d878a77ffc666d0ab96647568683b
IrunguMunene <irungu.munene@gmail.com>
2020-12-25 19:07:49 +0300

    One last commit. The project mainly was to test android development tools ACode, a code editor, and termux, an android terminal that allows us to install nodejs, git, and access code files through ftp from the ACode code editor. In the react app the main focus was testing implementation of functional components.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,69 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const App = () => {
-  return <Greetings />
-};
-
-const Greetings = () => {
+  
   const [greeting, setGreeting] = useState('');
+  
+  const [count, setCount] = useState(0);
   
   const handleGreetingChange = event => setGreeting(event.target.value);
   
-  return(
+  const handleCountIncreament = () => setTimeout(() => setCount(currentCount => currentCount + 1), 1000);
+  
+  const handleCountDecreament = () => setTimeout(() => setCount(currentCount => currentCount > 0 ? currentCount - 1 : 0), 1000);
+  
+  const sayHello = () => console.log('Hello');
+  
+  useEffect(() => setCount(currentCount => currentCount + 1), []);
+
+  return (
     <div>
-      <p>{greeting}</p>
+      <Greetings greeting = {greeting} />
       
-      <input
-        type = "text"
-        value = {greeting}
-        onChange = {handleGreetingChange}
-      />
+      <Counter count= {count} />
       
+      <Input value = {greeting} onChangeInput = {handleGreetingChange}>
+        Set Greeting:
+      </Input>
+      <Button handleClick = {sayHello}>
+        Testing
+      </Button>
+      
+      <Button handleClick = {handleCountIncreament}>
+        Increase Count
+      </Button>
+      
+      <Button handleClick = {handleCountDecreament}>
+        Decrease Count
+      </Button>
     </div>
   );
+};
+
+const Greetings = ({greeting}) =>
+  <p>{greeting}</p>;
+ 
+const Counter = ({count}) => <p>{count}</p>;
+
+// An abstract input component.
+const Input = ({value, onChangeInput, children}) => (
+  <label>
+    {children}
+    <input type = "text" value = {value} onChange = {onChangeInput} />
+  </label>
+);
+
+// An abstract button. Assigning default value in the function signature for destructuring to override component function.
+const Button = ({handleClick  = () => console.log('Default'), children}) => (
+  <button type = "button" onClick = {handleClick}>
+    {children}
+  </button>
+);
+
+Greetings.propTypes = {
+  value: PropTypes.string.isRequired,
 };
 
 export default App;
